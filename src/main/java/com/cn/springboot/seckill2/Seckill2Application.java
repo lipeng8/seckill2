5627a9dfe2367452a05747e0fe552e1f6b1b7f19
lipeng <lipeng8@asiainfo.com>
2019-07-19 14:52:04 +0800

    第一次提交

---
+++ src/main/java/com/cn/springboot/seckill2/Seckill2Application.java
@@ -0,0 +1,13 @@
+package com.cn.springboot.seckill2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seckill2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Seckill2Application.class, args);
+    }
+
+}
